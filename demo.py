b4a4edca9540d1f838a5a93d99ad84d40257dc52
Simmo Saan <simmo.saan@gmail.com>
2019-02-26 17:13:44 +0200

    Add human readable bytes formatting

--- demo.py
+++ demo.py
@@ -1,3 +1,5 @@
+import math
+
 import dotenv
 import os
 
@@ -12,6 +14,13 @@ stats = tplink.get_stats(5)
 dhcp = tplink.get_dhcp()
 hostnames = {entry.ip: entry.hostname for entry in dhcp}
 
+
+def format_bytes(b):
+    units = ["B", "kB", "MB", "GB", "TB"]
+    i = math.floor(math.log(b, 1024)) if b > 0 else 0
+    return f"{round(b / 1024 ** i * 100) / 100} {units[i]}"
+
+
 for entry in sorted(stats, key=lambda entry: entry.ip):
     hostname = hostnames.get(entry.ip)
-    print(f"{entry.ip} ({hostname}): {entry.bytes_total} {entry.bytes_per_sec}")
+    print(f"{entry.ip} ({hostname}): {format_bytes(entry.bytes_total)} {format_bytes(entry.bytes_per_sec)}/s")
