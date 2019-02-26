b03a242c802d4c0b03aea1d51541a22656375e67
Simmo Saan <simmo.saan@gmail.com>
2019-02-26 16:08:35 +0200

    Initial commit

---
+++ demo.py
@@ -0,0 +1,37 @@
+import dotenv
+import os
+import requests_toolbelt.sessions
+import lxml.html
+import json
+
+dotenv.load_dotenv()
+
+s = requests_toolbelt.sessions.BaseUrlSession(base_url="http://" + os.getenv("TPLINK_ADDRESS"))
+s.auth = (os.getenv("TPLINK_USERNAME"), os.getenv("TPLINK_PASSWORD"))
+
+r = s.get("/userRpm/SystemStatisticRpm.htm", params={"Num_per_page": 100})
+root = lxml.html.fromstring(r.content)
+
+stat_script = root.xpath("//script")[0].text
+stat_json = stat_script.replace("var statList = new Array(", "[").replace(");", "]")
+stat_list = json.loads(stat_json)
+
+
+def grouper(iterable, n):
+    args = [iter(iterable)] * n
+    return zip(*args)
+
+
+for row in grouper(stat_list, 13):
+    print(row)
+
+
+r = s.get("/userRpm/AssignedIpAddrListRpm.htm")
+root = lxml.html.fromstring(r.content)
+
+dhcp_script = root.xpath("//script")[0].text
+dhcp_json = dhcp_script.replace("var DHCPDynList = new Array(", "[").replace(");", "]")
+dhcp_list = json.loads(dhcp_json)
+
+for row in grouper(dhcp_list, 4):
+    print(row)
